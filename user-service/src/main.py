f621bcfa5d787df9ab987448bb1303834e8f7f86
mifa43 <mifa43kotez@gmail.com>
2021-05-19 12:33:24 +0200

    kreiran main fail sa helth check

---
+++ user-service/src/main.py
@@ -0,0 +1,16 @@
+import logging
+import uvicorn
+from fastapi import FastAPI
+
+logger = logging.getLogger(__name__)
+logger.setLevel("DEBUG")
+
+app = FastAPI()
+
+
+@app.get("/health")
+async def index():
+    return {"HEALTH" : "OK"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080, loop="asyncio")
