7b904e804f17da825f87e804ebaf5b9e80a32b2a
mifa43 <mifa43kotez@gmail.com>
2021-05-30 13:06:30 +0200

    kreirane su dve klase jedna sa upitima druga je test konekcija, definicja insert upisuje korisnika putem jsona - sledeci korak gadjanje register-api-a.

--- src/crud.py
+++ src/crud.py
@@ -1,18 +1,38 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import scoped_session, sessionmaker, Session
+from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
 import psycopg2
 import os
-
+from schemas import PersonalUserBase
 
 class PersonalUser_db():
-    def __init__(self):
-        pass
+    def __init__(self, name, surname, email, age, city, about_me):
+        self.name = name
+        self.surname = surname
+        self.email = email
+        self.age = age
+        self.city = city
+        self.about_me = about_me
+    def insert_user(self):
+        connection = psycopg2.connect(dbname=os.getenv("POSTGRES_DB"), user=os.getenv("POSTGRES_USER"), host=os.getenv("POSTGRES_HOST"), password=os.getenv("POSTGRES_PASSWORD"))
+        cur = connection.cursor()
+        query = """insert into udomi_ljubimca.personal_users ( name, surname, email, age, city, about_me)
+             values('{0}', '{1}', '{2}', '{3}', '{4}', '{5}')""".format(self.name , self.surname, self.email, self.about_me, self.city, self.age)
 
-    def insert_user():
-        pass
+        try:
+            cur.execute(query)
+            connection.commit()
+            cur.close()
+            connection.close() 
+            return {"message" : "the user is registered"}
 
+        except psycopg2.OperationalError as e:
+            raise e
+class TestDB:
     def db_conn_check():
         try:
             conn = psycopg2.connect(dbname=os.getenv("POSTGRES_DB"), user=os.getenv("POSTGRES_USER"), host=os.getenv("POSTGRES_HOST"), password=os.getenv("POSTGRES_PASSWORD"))
+            cursor = conn.cursor()
             conn.close()
             return {"HEALTH" : "OK"}
         except:
