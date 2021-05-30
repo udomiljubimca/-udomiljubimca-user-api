79f8272f0ef92daf52cc3f167a749c82ecd8cc20
mifa43 <mifa43kotez@gmail.com>
2021-05-30 13:07:00 +0200

    dva endpointa test-konekcija i upisivanje usera

--- src/main.py
+++ src/main.py
@@ -1,20 +1,25 @@
 import logging
 import uvicorn
 from fastapi import FastAPI, status
-from crud import PersonalUser_db
-
+from crud import PersonalUser_db, TestDB
+from schemas import PersonalUserBase
+from pydantic import BaseModel
 logger = logging.getLogger(__name__)
 logger.setLevel("DEBUG")
 
 app = FastAPI()
 
+@app.post("/insert-user")
+async def asasign_userDB(item : PersonalUserBase):
+    status = PersonalUser_db(item.name, item.surname, item.email, item.about_me, item.city, item.age).insert_user()
+    return {"message" : status["message"]}
 @app.get("/health")
 async def index():
     return {"HEALTH" : "OK"}
 
 @app.get("/health-db")
 async def testdb():
-    test_conn = PersonalUser_db.db_conn_check()
+    test_conn = TestDB.db_conn_check()
     if test_conn["HEALTH"] == "OK":
         return test_conn
     else:
