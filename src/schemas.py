d6fc6b92751c0bf1f14d6d99686acfc8bff19d0a
mifa43 <mifa43kotez@gmail.com>
2021-06-01 20:08:24 +0200

    modeli za polja

---
+++ src/schemas.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+
+class PersonalUserBase(BaseModel):
+    name: str
+    surname: str
+    email: str
+    about_me: str
+    city: str
+    age: int
+    terms_and_condition_accepted : str 
